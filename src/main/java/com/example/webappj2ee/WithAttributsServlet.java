1de06d63b1ef2e6eaf849ed838a6f4d2d8770068
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2023-12-23 16:55:12 +0100

    add - first exercice display attributs func

--- src/main/java/com/example/webappj2ee/WithAttributsServlet.java
+++ src/main/java/com/example/webappj2ee/WithAttributsServlet.java
@@ -1,2 +1,24 @@
-package com.example.webappj2ee;public class WithAttributsServlet {
+package com.example.webappj2ee;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class WithAttributsServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String nom = "Utilisateur";
+        Integer age = 20;
+
+        request.setAttribute("nom", nom);
+        request.setAttribute("age", age);
+
+        this.getServletContext().getRequestDispatcher("/vue/jspWithAttributs.jsp").forward( request, response );
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
