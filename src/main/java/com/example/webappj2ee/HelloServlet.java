3e42079273b97c90c655dd288600b906f2e421b8
StopThatTalace <117742366+StopThatTalace@users.noreply.github.com>
2023-12-23 16:55:51 +0100

    add - helloworld

--- src/main/java/com/example/webappj2ee/HelloServlet.java
+++ src/main/java/com/example/webappj2ee/HelloServlet.java
@@ -1,25 +1,20 @@
 package com.example.webappj2ee;
 
-import java.io.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+import java.io.IOException;
+
+@WebServlet(name = "served", value = "/served")
 public class HelloServlet extends HttpServlet {
-    private String message;
-
-    public void init() {
-        message = "Hello World!";
-    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+        String contextPath = request.getContextPath();
+
+        response.getWriter().write("Served at: " + contextPath);
     }
 
     public void destroy() {
